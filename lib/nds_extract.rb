25f597c0278a794c086236341058bd9b7f6cb5fd
Keenan Jones <keenanjones210@gmail.com>
2020-01-01 18:06:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,33 +15,27 @@ total_gross = 0
   total_gross
 end
 
-#understanding the method 
-#gross_for_director 
-#returns the total_gross for a director
-#it replaces the while loop
-#variables it needs to run are movies, directors and the nds 
-#gross_for_director(diri_movies, total_gross)
 
 def directors_totals(nds)
   my_hash = {}
   i_directors = 0
   
   while i_directors < nds.length do 
-    total_gross = 0
-    i_movies = 0
-    #if directors_name was an array and it saved each name as it looped
-    #Would that make it searchable
-    directors_name = nds[i_directors][:name]
-    
-    #make directors_name searchable
-    while i_movies < nds[i_directors][:movies].length do 
-      movie_gross = nds[i_directors][:movies][i_movies][:worldwide_gross]
-      total_gross += movie_gross
-      i_movies += 1 
-    end
-    my_hash[directors_name] = total_gross
-    i_directors += 1 
-    
+  total_gross = 0
+  i_movies = 0
+  #if directors_name was an array and it saved each name as it looped
+  #Would that make it searchable
+  directors_name = nds[i_directors][:name]
+  
+  #make directors_name searchable
+  while i_movies < nds[i_directors][:movies].length do 
+  movie_gross = nds[i_directors][:movies][i_movies][:worldwide_gross]
+  total_gross += movie_gross
+  i_movies += 1 
+  end
+  my_hash[directors_name] = total_gross
+  i_directors += 1 
+  
   end
   
   my_hash
