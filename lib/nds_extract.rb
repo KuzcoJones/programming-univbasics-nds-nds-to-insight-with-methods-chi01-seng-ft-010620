c2563f73e7abf34c2c33237df2d2538dfd9662c8
Keenan Jones <keenanjones210@gmail.com>
2020-01-01 18:05:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,6 +3,7 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+
 def gross_for_director(director_data) 
   #loops through directors data to give the 
   #total gross from a director
